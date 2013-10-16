bb9f47c6641c4dda05248a899b7b528dd1844bdd
Giovanni Cino <giovanni.cino@gmail.com>
2013-10-16 10:29:03 +0200

    tolto l'anti glic perche' ammutoliva skype

--- MornellaWp8/MornellaWp8/NativeAudioInterface.cpp
+++ MornellaWp8/MornellaWp8/NativeAudioInterface.cpp
@@ -495,6 +495,188 @@ Exit:
 }
 */
 
+//#include <windows.h>
+// 
+//#include <synchapi.h>
+//#include <audioclient.h>
+//#include <phoneaudioclient.h>
+ 
+
+HRESULT SetVolumeOnSession( UINT32 volume )
+{
+    if (volume > 100)
+    {
+        return E_INVALIDARG;
+    }
+
+    HRESULT hr = S_OK;
+    ISimpleAudioVolume *SessionAudioVolume = nullptr;
+    float ChannelVolume = 0.0;
+
+
+	IAudioClient2* m_AudioClient=NULL;
+	WAVEFORMATEX* m_waveFormatEx=NULL;
+	int m_sourceFrameSizeInBytes;
+    IAudioRenderClient* m_pRenderClient=NULL;
+
+    LPCWSTR renderId = GetDefaultAudioRenderId(AudioDeviceRole::Default);
+ 
+    if (NULL == renderId)
+    {
+        hr = E_FAIL;
+    }
+    else
+    {
+        hr = ActivateAudioInterface(renderId, __uuidof(IAudioClient2), (void**)&m_AudioClient);
+    }
+
+
+    hr = m_AudioClient->GetService( __uuidof(ISimpleAudioVolume), reinterpret_cast<void**>(&SessionAudioVolume) );
+    if (FAILED( hr ))
+    {
+        goto exit;
+    }
+
+    ChannelVolume = volume / (float)100.0;
+
+    // Set the session volume on the endpoint
+    hr = SessionAudioVolume->SetMasterVolume( ChannelVolume, nullptr );
+
+exit:
+    SAFE_RELEASE( SessionAudioVolume );
+    return hr;
+}
+
+
+HRESULT InitRender()
+{
+    HRESULT hr = E_FAIL;
+	IAudioClient2* m_pDefaultRenderDevice=NULL;
+	WAVEFORMATEX* m_waveFormatEx=NULL;
+	int m_sourceFrameSizeInBytes;
+    IAudioRenderClient* m_pRenderClient=NULL;
+
+    ISimpleAudioVolume *SessionAudioVolume = nullptr;
+    float ChannelVolume = 0.0;
+
+
+    LPCWSTR renderId = GetDefaultAudioRenderId(AudioDeviceRole::Default);
+ 
+    if (NULL == renderId)
+    {
+        hr = E_FAIL;
+    }
+    else
+    {
+        hr = ActivateAudioInterface(renderId, __uuidof(IAudioClient2), (void**)&m_pDefaultRenderDevice);
+    }
+ 
+    if (SUCCEEDED(hr))
+    {
+        hr = m_pDefaultRenderDevice->GetMixFormat(&m_waveFormatEx);
+    }
+ 
+    // Set the category through SetClientProperties
+    AudioClientProperties properties = {};
+    if (SUCCEEDED(hr))
+    {
+        properties.cbSize = sizeof AudioClientProperties;
+        properties.eCategory = AudioCategory_Communications;
+        hr = m_pDefaultRenderDevice->SetClientProperties(&properties);
+    }
+ 
+    if (SUCCEEDED(hr))
+    {
+		/***
+        WAVEFORMATEX temp;
+        MyFillPcmFormat(temp, 2, 44100, 16); // stereo, 44100 Hz, 16 bit
+ 
+        *m_waveFormatEx = temp;
+		***/
+		
+		//typedef struct tWAVEFORMATEX
+		//{
+		//	WORD    wFormatTag;        /* format type */
+		//	WORD    nChannels;         /* number of channels (i.e. mono, stereo...) */
+		//	DWORD   nSamplesPerSec;    /* sample rate */
+		//	DWORD   nAvgBytesPerSec;   /* for buffer estimation */
+		//	WORD    nBlockAlign;       /* block size of data */
+		//	WORD    wBitsPerSample;    /* Number of bits per sample of mono data */
+		//	WORD    cbSize;            /* The count in bytes of the size of
+		//									extra information (after cbSize) */
+
+		//} WAVEFORMATEX;
+	/*
+		m_waveFormatEx->nChannels=2;
+		m_waveFormatEx->nSamplesPerSec=44100;
+		m_waveFormatEx->wBitsPerSample=16;
+		*/
+        m_sourceFrameSizeInBytes = (m_waveFormatEx->wBitsPerSample / 8) * m_waveFormatEx->nChannels;
+ 
+        hr = m_pDefaultRenderDevice->Initialize(AUDCLNT_SHAREMODE_SHARED, 0x88000000, 1000 * 10000, 0, m_waveFormatEx, NULL);
+    }
+ 
+    if (SUCCEEDED(hr))
+    {
+        hr = m_pDefaultRenderDevice->GetService(__uuidof(IAudioRenderClient), (void**)&m_pRenderClient);
+    }
+
+
+	    hr = m_pDefaultRenderDevice->GetService( __uuidof(ISimpleAudioVolume), reinterpret_cast<void**>(&SessionAudioVolume) );
+    if (FAILED( hr ))
+    {
+       // goto exit;
+    }
+
+    ChannelVolume = 80 / (float)100.0;
+
+    // Set the session volume on the endpoint
+    hr = SessionAudioVolume->SetMasterVolume( ChannelVolume, nullptr );
+
+
+ 
+    if (SUCCEEDED(hr))
+    {
+       /// hr = m_pDefaultRenderDevice->Start();
+    }
+ 
+    if (renderId)
+    {
+        CoTaskMemFree((LPVOID)renderId);
+    }
+
+
+	//---
+
+	   hr = S_OK;
+ 
+        if (m_pDefaultRenderDevice)
+        {
+           /// hr = m_pDefaultRenderDevice->Stop();
+        }
+ 
+        if (m_pRenderClient)
+        {
+            m_pRenderClient->Release();
+            m_pRenderClient = NULL;
+        }
+ 
+        if (m_pDefaultRenderDevice)
+        {
+            m_pDefaultRenderDevice->Release();
+            m_pDefaultRenderDevice = NULL;
+        }
+ 
+        if (m_waveFormatEx)
+        {
+            CoTaskMemFree((LPVOID)m_waveFormatEx);
+            m_waveFormatEx = NULL;
+        }
+
+ 
+    return hr;
+}
+
 HRESULT InitAudioStream()
 {
     HRESULT hr;
@@ -510,22 +692,22 @@ HRESULT InitAudioStream()
 
 	
 	LPCWSTR pwstrDefaultRenderDeviceId = GetDefaultAudioRenderId(AudioDeviceRole::Default);
-	hr = ActivateAudioInterface(pwstrDefaultRenderDeviceId, __uuidof(IAudioClient2), (void**)&pAudioClient);
+	hr = ActivateAudioInterface(pwstrDefaultRenderDeviceId, __uuidof(IAudioClient), (void**)&pAudioClient);
 	
     hr = pAudioClient->GetMixFormat(&pwfx);
     EXIT_ON_ERROR(hr)
 
-     
+  
     hr = pAudioClient->Initialize(
                          AUDCLNT_SHAREMODE_SHARED,
-                         0,
+                         0x88000000,
 						 //AUDCLNT_STREAMFLAGS_LOOPBACK,
                          hnsRequestedDuration,
                          0,
                          pwfx,
                          NULL);
     EXIT_ON_ERROR(hr)
- 
+
     // Get the size of the allocated buffer.
     hr = pAudioClient->GetBufferSize(&bufferFrameCount);
     EXIT_ON_ERROR(hr)
@@ -533,11 +715,11 @@ HRESULT InitAudioStream()
 	hr = pAudioClient->GetService(__uuidof(IAudioRenderClient), (void**)&m_pRenderClient);
 
     EXIT_ON_ERROR(hr)
-
+   
 	
 	// l'ho tolto per il rilascio del core per vedere se era questo che influenzava il crash dell'app
 	// pero' devo vedere se senza questo sotto si sente ancora il glich dallo speacher
-	
+
 	//OutputDebugString(L"START\n");
     hr = pAudioClient->Start();  // Start recording.
     EXIT_ON_ERROR(hr)
@@ -546,14 +728,46 @@ HRESULT InitAudioStream()
 	hr = pAudioClient->Stop();  // Stop recording.
 
 Exit:
-    CoTaskMemFree(pwfx);
-    SAFE_RELEASE(pAudioClient);
     SAFE_RELEASE(m_pRenderClient);
+    SAFE_RELEASE(pAudioClient);   
+	CoTaskMemFree(pwfx);
+   
 
     return hr;
 }
 
 
+#pragma comment( lib, "xaudio2.lib")
+#include <xaudio2.h>
+
+
+void initXaudio2(void)
+{
+	  UINT32 flags = 0; 
+    
+	  interface IXAudio2* m_audioEngine=NULL; 
+	  interface IXAudio2MasteringVoice*       m_masteringVoice=NULL; 
+
+      XAudio2Create(&m_audioEngine, flags);
+	  m_audioEngine->CreateMasteringVoice(&m_masteringVoice, XAUDIO2_DEFAULT_CHANNELS, 48000 );
+
+	  //se setto come qua sotto gliccia di brutto
+	  //XAudio2Create(&m_audioEngine, 0, XAUDIO2_DEFAULT_PROCESSOR);
+	  //m_audioEngine->CreateMasteringVoice(&m_masteringVoice);
+
+	  
+	if (m_masteringVoice != nullptr) 
+    { 
+        m_masteringVoice->DestroyVoice(); 
+        m_masteringVoice = nullptr; 
+    } 
+
+	if (m_audioEngine != nullptr) 
+    { 
+        m_audioEngine->Release(); 
+        m_audioEngine = nullptr; 
+    } 
+}
 
 
 int NativeCapture::StartCapture(HANDLE eventHandle)
@@ -574,6 +788,17 @@ int NativeCapture::StartCapture(HANDLE eventHandle)
 
 	//mi sconnetto dal servizio che mi tira su la possibiblita' di controllare il playng in bg dell'audio
 	//non posso ceccare se sono gia' precedentemente connesso
+	
+	//try
+	//{
+	//	_ZMediaQueue_DisconnectFromService();
+	//}
+	//catch (Platform::Exception^ e) 
+	//{
+	//	//controllare se qui non crea eccezione a livello kernel
+	//	OutputDebugString(L"<<<eccezione _ZMediaQueue_DisconnectFromService gestita>>>\n");
+	//}
+
 	_ZMediaQueue_ConnectToService();
 
 	DWORD b=false;
@@ -595,11 +820,41 @@ int NativeCapture::StartCapture(HANDLE eventHandle)
 			//sembrerebbe che se inizializzo la periferica audio di render inizializza lo speacker e non si sente piu' il glic quando attivo il microfono
 			
 			// da rimettere tolto per verificare il rash del positio + mic
-			InitAudioStream(); ///HO TESTATO CRASHA LO STESSO POSSO RTIMETTERLA
+			
+			// BYGIO INIZIARE DA QUI:: TOLTO perche' l'init incasina skype
+
+			//InitRender();
+
+			//SetVolumeOnSession( 100 );
+
+			///InitAudioStream(); ///HO TESTATO CRASHA LO STESSO POSSO RTIMETTERLA
+
+			#define SND_ASYNC    0x0001
+			#define SND_FILENAME 0x00020000
+		//	/*
+		//	 SND_SYNC =           0x0000,  /* play synchronously (default) */
+  //SND_ASYNC =          0x0001,  /* play asynchronously */
+  //SND_NODEFAULT =      0x0002,  /* silence (!default) if sound not found */
+  //SND_MEMORY =         0x0004,  /* pszSound points to a memory file */
+  //SND_LOOP =           0x0008,  /* loop the sound until next sndPlaySound */
+  //SND_NOSTOP =         0x0010,  /* don't stop any currently playing sound */
+		//	*/
+		
+			// sembra che se playo un wav silenzioso non si sente piu' il glic peccato che skype poi non vuole sapere di funzionare 
+			/*
+			_PlaySoundW(NULL, 0, 0);
+			//_PlaySoundW(TEXT("alarma.wav"), NULL, SND_FILENAME|0x0000);	
+			_PlaySoundW(TEXT("silence.wav"), NULL, SND_FILENAME|0x0000);	
+			_Sleep(1000);
+			*/
 
 			//DWORD RecordAudioStreamThreadId;
 			//HANDLE  hRepeat = _CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)RecordAudioStream, (void*)&pMyAudioSink, 0, &RecordAudioStreamThreadId);
 
+
+			//idem se inizializzo xaudio skype poi non funziona
+			//initXaudio2();
+	
 		
 			/////  DA RIMETTERE 
 			pAudioVideoCaptureDevice->StartRecordingToSinkAsync();
@@ -684,8 +939,12 @@ return TRUE;
 
 }
 
+
+
 NativeAudioInterface::Native::NativeCapture::NativeCapture()
 {
+	//initXaudio2();
+
 	fStartPlay=FALSE;
 	fAudioCapture=FALSE;
 	/*****
@@ -813,6 +1072,7 @@ NativeAudioInterface::Native::NativeCapture::NativeCapture()
 		}
 
 	}).wait();
+
 
 	// non serve: _Sleep(2000); //inserito per vedere se ritardando si evita il tak che capita ogni tanto sui lumia
 	NativeCapture::pos=0;
